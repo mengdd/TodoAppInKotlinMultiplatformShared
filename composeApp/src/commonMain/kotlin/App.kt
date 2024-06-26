5872f9a4ab8ca47e38353046821dd971589483c6
Dandan Meng <mengdandanno1@163.com>
2024-06-26 09:15:55 +0800

    todo list ui

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,33 +1,69 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import todoappinkotlinmultiplatformshared.composeapp.generated.resources.Res
-import todoappinkotlinmultiplatformshared.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+        Column(
+            Modifier.fillMaxWidth().padding(8.dp),
+            horizontalAlignment = Alignment.Start,
+        ) {
+            val todoList = remember { mutableStateListOf<TodoItem>() }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                var text by remember { mutableStateOf("") }
+
+                OutlinedTextField(
+                    singleLine = true,
+                    modifier = Modifier.weight(1.0f),
+                    value = text,
+                    onValueChange = { text = it },
+                    label = { Text("Todo") }
+                )
+                Button(modifier = Modifier.padding(8.dp),
+                    onClick = {
+                        if (text.isNotBlank()) {
+                            todoList.add(TodoItem(text))
+                        }
+                        text = ""
+                    }) {
+                    Text("Add")
+                }
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+
+            LazyColumn(
+                Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.Start
+            ) {
+                items(todoList) {
+                    Text(
+                        modifier = Modifier.padding(8.dp),
+                        text = it.title,
+                        style = MaterialTheme.typography.h4
+                    )
                 }
             }
         }
---
+++ composeApp/src/commonMain/kotlin/TodoList.kt
@@ -0,0 +1,1 @@
+data class TodoItem(val title: String)
